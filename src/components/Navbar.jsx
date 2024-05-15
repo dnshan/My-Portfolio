7ce668ea152dc000cbfa16079fe7e4afe901a331
dnshan <dnshan46@gmail.com>
2024-05-15 16:17:06 +0530

    Inital Commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import logo from "../assets/Dn.png"
+import { FaLinkedin } from "react-icons/fa"
+import { FaGithub } from "react-icons/fa"
+import { FaInstagram } from "react-icons/fa"
+
+
+const Navbar = () => {
+  return (
+    <nav className="flex items-center justify-between py-6">
+    <div className="flex flex-shrink-0 items-center">
+      <img className="mx-2 w-10" src={logo} alt="logo" />
+    </div>
+    <div className="m-1 flex items-center justify-center gap-4 text-2xl">
+        <FaLinkedin/>
+        <FaGithub/>
+        <FaInstagram/>
+    </div>
+    </nav>
+  )
+}
+
+export default Navbar
